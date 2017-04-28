fdf87e4cea4920272ae79cb5bdce5c8bacf83c60
ssbks55 <ssbks55@gmail.com>
2017-04-28 11:52:16 +0530

    First Commit

---
+++ CacheBrowser/default.aspx
@@ -0,0 +1,122 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CacheBrowser.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <style type="text/css">
+        .txtBox
+        {
+            width: 700px;
+            padding: 5px;
+        }
+
+        .btnBox
+        {
+            padding: 2px;
+            width: 140px;
+        }
+
+        .frmBox
+        {
+            width: 100%;
+            height: 100%;
+            overflow-x: auto;
+        }
+
+        .sfbgg
+        {
+            background-color: #f1f1f1;
+            border-bottom: 1px solid #e5e5e5;
+            border-color: #e5e5e5;
+            height: 27px;
+            padding: 5px;
+            margin-top: -5px;
+        }
+
+
+
+        .kpbb
+        {
+            background-color: #4d90fe;
+            background-image: -moz-linear-gradient(center top, #4d90fe, #4787ed);
+            border: 1px solid #3079ed;
+            padding: 4px;
+            width: 100px;
+            color: #fff;
+        }
+
+        .logo
+        {
+            color: cadetblue;
+            font-size: 14px;
+            font-weight: bolder;
+            width: 10px;
+        }
+
+        .bar
+        {
+            float: left;
+            margin-top: 5px;
+            width: 140px;
+        }
+
+        .txtTokenBox
+        {
+            width: 70px;
+            padding: 5px;
+        }
+    </style>
+    <script type="text/javascript">
+        function refresh() {
+            $("[id$=iframeViewer]")[0].contentWindow.location.reload();
+        }
+
+        $("[id$=iframeViewer]").load(function () {
+            alert('frame has (re)loaded');
+        });
+
+        function remove() {
+            debugger;
+            if ($("[id$=iframeViewer]").contents().find('#google-cache-hdr').length > 0)
+                $("[id$=iframeViewer]").contents().find('#google-cache-hdr')[0].style.display = "none";
+            
+        }
+
+        //$(document).ready(function () {
+        //    $("[id$=chkSecure]").change(function (evnt) {
+        //        if (this.checked) {
+        //            alert("Currently available for premium user's only.");
+        //            event.pr
+        //        }
+        //    });
+        //});
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="sfbgg">
+            <div class="bar">
+                <span class="logo">Koogle</span>
+                <span style="font-size: 12px">alpha</span>
+            </div>
+         <%--   <asp:CheckBox Text="" ToolTip="Enable secure search" ID="chkSecure" runat="server" />--%>
+            <asp:TextBox runat="server" CssClass="txtBox" ID="txtUrl"></asp:TextBox>
+            <asp:TextBox runat="server" ID="txtToken" TextMode="Password" MaxLength="3" Visible="false" CssClass="txtTokenBox"></asp:TextBox>
+            <asp:Button runat="server" CssClass="kpbb" OnClick="btnSubmit_Click" ID="btnSubmit" Text="Search" />
+
+            <span>IP Address:</span>
+            <asp:Label ForeColor="Green" ID="lblIP" runat="server" />
+            <span>and Host Name:</span>
+            <asp:Label ForeColor="Green" ID="lblHostName" runat="server" />
+        </div>
+        <div style="height: 575px">
+            <asp:Label ForeColor="Red" Visible="false" ID="lblError" Text="" runat="server" />
+            <iframe id="iframeViewer" runat="server" class="frmBox" frameborder="0"></iframe>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CacheBrowser/default.aspx.cs
@@ -0,0 +1,135 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CacheBrowser
+{
+    public partial class Home : System.Web.UI.Page
+    {
+
+        #region Properties
+        public List<String> blockedIPAddresses
+        {
+            get
+            {
+                if (ViewState["blockedIPAddresses"] != null)
+                {
+                    return (ViewState["blockedIPAddresses"]) as List<String>;
+                }
+                return new List<String>();
+            }
+            set
+            {
+                ViewState["blockedIPAddresses"] = value;
+            }
+        } 
+        #endregion
+
+        #region Page Events
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblIP.Text = Request.UserHostAddress;
+                lblHostName.Text = Request.UserHostName;
+
+                blockedIPAddresses = System.IO.File.ReadAllLines("\\CacheBrowser\\Resources\\BlockedIPAddresses.txt").ToList();
+                if (blockedIPAddresses.Contains(Request.UserHostAddress))
+                {
+                    lblError.Text = "Your IP is blocked. Contact Administrator.";
+                    lblError.Visible = true;
+                    txtUrl.Enabled = false;
+                    btnSubmit.Enabled = false;
+                }
+            }
+        } 
+        #endregion
+
+        #region Control Events
+
+        /// <summary>
+        /// Search button 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ip = Request.UserHostAddress;
+                string hostname = Request.UserHostName;
+
+                WriteToFile("------------------------------------------Koogle Log ---" + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt") + "-------------------------------------- ");
+                WriteToFile("Search Query: " + txtUrl.Text.Trim());
+                WriteToFile(String.Format("IP: {0} HostName: {1}", ip, hostname));
+                if (!blockedIPAddresses.Contains(Request.UserHostAddress))
+                {
+
+                    lblError.Visible = false;
+                    iframeViewer.Src = string.Empty;
+                    iframeViewer.Visible = false;
+                    string url = "http://webcache.googleusercontent.com/search?q=cache:" + txtUrl.Text.Trim().ToLower();
+                    string path = "~/Dummy.html";
+                    File.WriteAllText(Server.MapPath(path), String.Empty);
+                    var webRequest = WebRequest.Create(url);
+                    using (var response = webRequest.GetResponse())
+                    using (var content = response.GetResponseStream())
+                    using (var reader = new StreamReader(content))
+                    {
+                        String strContent = reader.ReadToEnd();
+                       // String[] asaa = Regex.Split(strContent, "<!DOCTYPE html>"); // strContent.Split("<!DOCTYPE");
+
+                        HtmlDocument resultat = new HtmlDocument();
+                        resultat.LoadHtml(strContent);
+                        resultat.GetElementbyId("google-cache-hdr").RemoveAll();
+                        using (StreamWriter _testData = new StreamWriter(Server.MapPath(path), true))
+                        {
+                            _testData.WriteLine(resultat.DocumentNode.OuterHtml); // Write the file.
+                        }
+                    }
+                    iframeViewer.Visible = true;
+                    iframeViewer.Src = "Dummy.html";
+                }
+
+            }
+            catch (Exception)
+            {
+                iframeViewer.Visible = true;
+                iframeViewer.Src = "Error.html";
+            }
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        private bool IsUserActivityLegal()
+        {
+            //todo...
+            return true;
+        }
+
+        /// <summary>
+        /// Create Log Files
+        /// </summary>
+        /// <param name="text"></param>
+        private void WriteToFile(string text)
+        {
+            string path = "\\CacheBrowser\\Resources\\KoogleUserSearchHistory.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(text);
+                writer.Close();
+            }
+        } 
+        #endregion
+    }
+}
